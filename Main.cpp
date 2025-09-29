d576a99a7447ca13675a97525d6183c77e3263cc
Squibl <jakob.xo@gmail.com>
2025-09-29 16:35:49 +0200

    init

---
+++ Main.cpp
@@ -0,0 +1,14 @@
+//Aufgabe1 : Objektorientierte Programmierung Praktikum A4 : V0 : 29.09.2025 : Jakob Onnen/ Liam Poppinga
+
+//Include Anweisungen
+#include <stdio.h>
+#include <math.h>
+
+//Klassen- und Schnittstellenfestlegungen
+
+
+int main(void){
+
+    printf("Hallo\n");
+    return 0;
+}
