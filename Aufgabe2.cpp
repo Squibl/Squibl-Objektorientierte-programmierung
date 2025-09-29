3c57c4ed1644d98ca50fb1b4e0d0dd1fdc584f76
Squibl <jakob.xo@gmail.com>
2025-09-29 18:58:04 +0200

    Aufgabe 2

--- Aufgabe2.cpp
+++ Aufgabe2.cpp
@@ -1,1 +1,16 @@
 //Aufgabe2 : Objektorientierte Programmierung Praktikum Gruppe A4 : V0 : 29.09.2025 : Jakob Onnen/ Liam Poppinga
+
+void aufgabe2(void){
+    const int rangeSize = 10;    //Anzahl der Zeiger
+    double* array[rangeSize];    //Array größe rangeSize
+    double count=0;
+
+    //Anhängen der Werte
+    for (int i = 0; i < count; i++)
+    {
+        array[i]= new double(i*1.1);
+    }
+    
+
+
+}
