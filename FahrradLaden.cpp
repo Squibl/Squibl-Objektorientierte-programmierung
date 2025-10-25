da789de9197d080502495bf60a799a4148f7b690
Squibl <jakob.xo@gmail.com>
2025-10-25 14:24:58 +0200

    Konstruktor_Init

--- FahrradLaden.cpp
+++ FahrradLaden.cpp
@@ -1,47 +1,51 @@
-//Fahrradladen : Objektorientierte Programmierung Praktikum A4 : V1 : 29.09.2025 : Jakob Onnen/ Liam Poppinga
+// Fahrradladen : Objektorientierte Programmierung Praktikum A4 : V1 : 29.09.2025 : Jakob Onnen/ Liam Poppinga
 
-//Include Anweisungen
+// Include Anweisungen
 #include <math.h>
 #include <iostream>
 #include <string>
 
 using namespace std;
 
-//Klassen- und Schnittstellenfestlegungen
-class Fahrrad{
-    private:
-        string marke;
-        string modell;
-        int modellJahr;
-        double preis;
-    public:
-        // Default Konstruktor    
-        Fahrrad() {
-            modellJahr=-1;
-            preis=-1;
-        }
-        // Konstruktor zur Initialisierung aller Werte
-        Fahrrad(string m) : Fahrrad(){marke=m;}
-        Fahrrad(string m, string mo) : Fahrrad(){} 
-        Fahrrad(string m, string mo, int jahr) : Fahrrad(m, mo){}
-        Fahrrad(string m, string mo, int jahr, double p) : Fahrrad(m, mo, jahr){}
+// Klassen- und Schnittstellenfestlegungen
+class Fahrrad
+{
+private:
+    string marke;
+    string modell;
+    int modellJahr;
+    double preis;
 
+public:
+    // Default Konstruktor
+    Fahrrad()
+    {
+        modellJahr = -1;
+        preis = -1;
+    }
+    // Konstruktor zur Initialisierung aller Werte
+    Fahrrad(string m) : Fahrrad() { marke = m; }
+    Fahrrad(string m, string mo) : Fahrrad(m) { modell = mo; }
+    Fahrrad(string m, string mo, int jahr) : Fahrrad(m, mo) { modellJahr = jahr; }
+    Fahrrad(string m, string mo, int jahr, double p) : Fahrrad(m, mo, jahr) { preis = p; }
 
-        // Get-Methoden
-        string getMarke() const { return marke; }
-        string getModell() const { return modell; }
-        int getModellJahr() const { return modellJahr; }
-        double getPreis() const { return preis; }
+    // Get-Methoden
+    string getMarke() const { return marke; }
+    string getModell() const { return modell; }
+    int getModellJahr() const { return modellJahr; }
+    double getPreis() const { return preis; }
 
-        // Methode zur Ausgabe von Marke und Modell
-        void nenneMarkeModell(void) const {
-            cout << "Marke: " << marke << ", Modell: " << modell << endl;
-        }
+    // Methode zur Ausgabe von Marke und Modell
+    void nenneMarkeModell(void) const
+    {
+        cout << "Marke: " << marke << ", Modell: " << modell << endl;
+    }
 };
 
-int main (void){
-    
-    Fahrrad Auto();
+int main(void)
+{
+
+    Fahrrad Auto;
     Auto.nenneMarkeModell();
     return 0;
 }
